27e9bd7b043841ea79aad46f9637fae71221b492
Alexey-Chekashev <hbagnxbbxj@gmail.com>
2023-03-20 18:54:56 +0300

    LR1-4_fixes

--- libs/spdlog/build/compile.sh
+++ libs/spdlog/build/compile.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #ABI=armaebi-v7a
-#ABI=x86
+ABI=x86
 #ABI=arm64-v8a
-ABI=x86_64
+#ABI=x86_64
 
-ANDROID_NDK=$HOME\\AppData\\Local\\Android\\Sdk\\ndk\\21.0.6113669
-TOOL_CHAIN=${ANDROID_NDK}\\build\\cmake\\android.toolchain.cmake
-CMAKE=$HOME\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\cmake.exe
+ANDROID_NDK=$HOME/Android/Sdk/ndk/25.2.9519653
+TOOL_CHAIN=${ANDROID_NDK}/build/cmake/android.toolchain.cmake
+CMAKE=$HOME/Android/Sdk/cmake/3.22.1/bin/cmake
 
 mkdir -p ${ABI}
 cd ${ABI}
 
-${CMAKE} -G Ninja ..\\..\\spdlog -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 \
--DANDROID_ABI=${ABI} -DCMAKE_TOOLCHAIN_FILE=${TOOL_CHAIN} -DCMAKE_MAKE_PROGRAM=ninja.exe
+${CMAKE} ../../spdlog -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 \
+-DANDROID_ABI=${ABI} -DCMAKE_TOOLCHAIN_FILE=${TOOL_CHAIN} -DCMAKE_CXX_FLAGS=-DSPDLOG_COMPILED_LIB
 
 ${CMAKE} --build .
