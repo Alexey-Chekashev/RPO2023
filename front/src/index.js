8a8066e12cb46f640013c7a8dc9956f539af6cf1
Alexey-Chekashev <hbagnxbbxj@gmail.com>
2023-04-22 19:03:33 +0300

    LR 8

---
+++ front/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <div className="App">
+          <BrowserRouter>
+              <NavigationBar />
+              <div className="container-fluid">
+                  <Routes>
+                      <Route path="home" element={<Home />}/>
+                  </Routes>
+              </div>
+          </BrowserRouter>
+      </div>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
